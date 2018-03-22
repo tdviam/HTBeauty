e3ce146c502a2c4eec6d849dc634970edf0e5785
viet <tdviam@gmail.com>
2018-03-22 10:31:04 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<? include 'header.php' ?>	
+<article class="module width_full">
+<header><h3 class="tabs_involved">Quản trị bài viết</h3>
+
+</header>
+
+<div class="tab_container">
+	<div id="tab1" class="tab_content">
+	
+	
+	</div><!-- end of #tab1 -->
+		
+</div><!-- end of .tab_container -->
+
+</article><!-- end of content manager article -->
+
+<div class="clear"></div>
+<? include 'footer.php' ?>	
