98500787b6eb7b8ca39ac315079f57031e7efc6d
tdviam <36843616+tdviam@users.noreply.github.com>
2018-03-23 17:20:52 +0700

    Update htbeauty.sql

--- htbeauty.sql
+++ htbeauty.sql
@@ -32,19 +32,6 @@ CREATE TABLE IF NOT EXISTS `tblproducts` (
   `fee` decimal(20,0) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblproducts`
---
-
-INSERT INTO `tblproducts` (`ID`, `email`, `fee`) VALUES
-('#100015', 'trangmax95@gmail.com', '670000'),
-('#100014', 'trangmax95@gmail.com', '542000'),
-('#100013', 'nguyenhien.tn96@gmail.com', '340250'),
-('#100012', 'hatrangshb@gmail.com', '254000'),
-('#100011', 'thuyflash@gmail.com', '206000'),
-('#100010', 'thuyflash@gmail.com', '250000'),
-('#100009', 'diamond609llq@gmail.com', '270000');
-
 -- --------------------------------------------------------
 
 --
@@ -55,22 +42,6 @@ CREATE TABLE IF NOT EXISTS `tblproduct_details` (
   `ID` varchar(30) NOT NULL,
   `product_name` mediumtext CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tblproduct_details`
---
-
-INSERT INTO `tblproduct_details` (`ID`, `product_name`) VALUES
-('#100015', 'Son kem BOURJOIS Velvet màu 08 Grand Cru - BOURJOIS'),
-('#100015', 'Son MAC Chili Matte Lipstick - MAC'),
-('#100014', 'Son Bourjois Velvet màu 19 Jolie-de-vin - BOURJOIS'),
-('#100014', 'Son MAC Russian Red Matte Lipstick - MAC'),
-('#100013', 'Kem tái tạo da tổn thương CICALFATE AVENE Crème Réparatrice Antibactérienne, 100ml - AVENE / -Chăm sóc toàn diện / -Mọi loại da'),
-('#100012', 'Xịt khoáng Vichy Eau Thermal 150ml - VICHY'),
-('#100012', 'Bông tẩy trang Emily Đức 120 pad - EMILY'),
-('#100011', 'Cặp 2 kem dưỡng tay và son dưỡng mật ong Nuxe Kit Rêve de Miel 1 Crème Mains 30mL+ 1 Stick Lèvres 4g - NUXE / -Da thiếu dầu, da mất nước, da khô / -Da khô'),
-('#100010', 'Cặp 2 kem dưỡng tay và son dưỡng mật ong Nuxe Kit Rêve de Miel 1 Crème Mains 30mL+ 1 Stick Lèvres 4g - NUXE / -Da thiếu dầu, da mất nước, da khô / -Da khô'),
-('#100009', 'Nước hoa Lady Million Paco Rabanne 10ml - PACO RABANNE');
 
 -- --------------------------------------------------------
 
